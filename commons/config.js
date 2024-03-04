e402eadd57e6c30806e0b9b4a1ff379da9e0d397
akshay pednekar <akshay.pednekar@gmail.com>
2024-03-04 01:22:08 -0600

    formatting code

--- commons/config.js
+++ commons/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 // Load environment variables based on NODE_ENV
 if (process.env.NODE_ENV === "production") {
-  dotenv.config({ path: ".env" });
+    dotenv.config({ path: ".env" });
 } else {
-  dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+    dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 }
