0fba6c1e3115ebd668ba1448848e5d32780347d7
Aditya Patange <contact.adityapatange@gmail.com>
2024-03-14 13:12:22 +0530

    backend: enable cors

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import "./commons/config.js";
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import { APP_NAME, APP_PORT, SERVICE_NAME } from "./commons/constants.js";
 import { FacultyManager, getFaculty } from "./services/s-service.js";
@@ -11,104 +12,105 @@ import { ner } from "./services/nlp-service.js";
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(timeout("300s"));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
 app.post(`/${SERVICE_NAME}/email`, async (req, res) => {
-    try {
-        const result = await generateEmail(req.body);
-        res.status(200).json({
-            result,
-        });
-    } catch (error) {
-        console.error(
-            {
-                id: `/${SERVICE_NAME}/email`,
-                message: "failed to generate email",
-            },
-            // printing the error stacktrace
-            error
-        );
-        //return response to client
-        res.status(500).json({ error: "failed to generate email" });
-    }
+  try {
+    const result = await generateEmail(req.body);
+    res.status(200).json({
+      result,
+    });
+  } catch (error) {
+    console.error(
+      {
+        id: `/${SERVICE_NAME}/email`,
+        message: "failed to generate email",
+      },
+      // printing the error stacktrace
+      error
+    );
+    //return response to client
+    res.status(500).json({ error: "failed to generate email" });
+  }
 });
 
 app.get(`/${SERVICE_NAME}/stanford/faculty`, async (req, res) => {
-    try {
-        const result = await getFaculty();
-        res.status(200).json({
-            result,
-        });
-    } catch (error) {
-        console.error(
-            {
-                id: `/${APP_NAME}/stanford/faculty`,
-                message: "failed to fetch faculty",
-            },
-            error
-        );
-        //return response to client
-        res.status(500).json({ error: "failed to fetch faculty" });
-    }
+  try {
+    const result = await getFaculty();
+    res.status(200).json({
+      result,
+    });
+  } catch (error) {
+    console.error(
+      {
+        id: `/${APP_NAME}/stanford/faculty`,
+        message: "failed to fetch faculty",
+      },
+      error
+    );
+    //return response to client
+    res.status(500).json({ error: "failed to fetch faculty" });
+  }
 });
 
 app.get(`/${SERVICE_NAME}/stanford/faculty-details`, async (req, res) => {
-    try {
-        const result = await getFacultyDetails(req.query.professorName);
-        res.status(200).json({
-            result,
-        });
-    } catch (error) {
-        console.error(
-            {
-                id: `/${SERVICE_NAME}/stanford/faculty-details`,
-                message: "failed to fetch faculty details",
-            },
-            error
-        );
-        //return response to client
-        res.status(500).json({ error: "failed to fetch faculty details" });
-    }
+  try {
+    const result = await getFacultyDetails(req.query.professorName);
+    res.status(200).json({
+      result,
+    });
+  } catch (error) {
+    console.error(
+      {
+        id: `/${SERVICE_NAME}/stanford/faculty-details`,
+        message: "failed to fetch faculty details",
+      },
+      error
+    );
+    //return response to client
+    res.status(500).json({ error: "failed to fetch faculty details" });
+  }
 });
 
 app.post(`/${SERVICE_NAME}/stanford/match`, async (req, res) => {
-    try {
-        // const faculty = FacultyManager.get();
-        // const result = await match(req.query.professorName, faculty);
-        const professorName = req.query.professorName;
-        const collaboratorName = req.query.collaboratorName;
+  try {
+    // const faculty = FacultyManager.get();
+    // const result = await match(req.query.professorName, faculty);
+    const professorName = req.query.professorName;
+    const collaboratorName = req.query.collaboratorName;
 
-        const faculty = await getFaculty();
-        const professor = await getFacultyDetails(professorName, faculty);
-        const collaborator = await getFacultyDetails(collaboratorName, faculty);
+    const faculty = await getFaculty();
+    const professor = await getFacultyDetails(professorName, faculty);
+    const collaborator = await getFacultyDetails(collaboratorName, faculty);
 
-        const nerResult = await ner(professor, collaborator);
-        const email =  await generateEmail(nerResult)
+    const nerResult = await ner(professor, collaborator);
+    const email = await generateEmail(nerResult);
 
-        res.status(200).json({
-            nerResult,
-            email
-        });
-    } catch (error) {
-        console.error(
-            {
-                id: `/${SERVICE_NAME}/stanford/match`,
-                message: "failed to find match",
-            },
-            error
-        );
-        //return response to client
-        res.status(500).json({ error: "failed to find match" });
-    }
+    res.status(200).json({
+      nerResult,
+      email,
+    });
+  } catch (error) {
+    console.error(
+      {
+        id: `/${SERVICE_NAME}/stanford/match`,
+        message: "failed to find match",
+      },
+      error
+    );
+    //return response to client
+    res.status(500).json({ error: "failed to find match" });
+  }
 });
 
 app.listen(APP_PORT, () => {
-    console.log(`${APP_NAME} app listening on port ${APP_PORT}`);
+  console.log(`${APP_NAME} app listening on port ${APP_PORT}`);
 });
