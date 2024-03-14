2a7769e855292037377d8b9f57d7fa73f77eb2d1
Aditya Patange <contact.adityapatange@gmail.com>
2024-03-14 12:34:38 +0530

    frontend: fix onSelect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,28 +3,33 @@ import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 
 function App() {
-    const options = ["one", "two", "three"];
-    const defaultOption = options[0];
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Opportunity Finder</h1>
+  const options = ["one", "two", "three"];
+  const defaultOption = options[0];
 
-                <p>
-                    Tool for helping professors collaborate and find similar
-                    research partners
-                </p>
-            </header>
-            <div>
-                <Dropdown
-                    options={options}
-                    onChange={this._onSelect}
-                    value={defaultOption}
-                    placeholder="Select an option"
-                />
-            </div>
-        </div>
-    );
+  const onSelect = (value) => {
+    console.log("onSelect", value);
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>Opportunity Finder</h1>
+
+        <p>
+          Tool for helping professors collaborate and find similar research
+          partners
+        </p>
+      </header>
+      <div>
+        <Dropdown
+          options={options}
+          onChange={onSelect}
+          value={defaultOption}
+          placeholder="Select an option"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
